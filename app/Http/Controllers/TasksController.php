ecffe465efba8de8d64b9b99ae7b7ef8de382b7d
keigo.kunitomo <keigo1573762@icloud.com>
2020-07-01 11:44:50 +0000

    pagination

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -16,7 +16,7 @@ class TasksController extends Controller
     public function index()
     {
          
-         $tasks=Task::all();
+         $tasks=Task::paginate(25);
          
          
          return view('tasks.index', [
